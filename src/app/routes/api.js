a0647a151cffa61d51c61ad8716b4559b72d8637
Nate <palmern22@gmail.com>
2015-08-30 20:50:36 -0400

    Cattle Crud

--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -15,8 +15,64 @@ router.get('/connect', function(req, res, next){
   res.status(200).json(Cattle.schema);
 })
 
-router.post('/add', function(req, res, next){
-  res.status(200).json(req.body);
+router.get('/remove/:id', function(req, res, next){
+  Cattle.findById(req.params.id, function(err, cattle){
+    if(err){
+      res.status(500).json(err);
+    }
+    cattle.remove(function(err){
+      if(err){
+        res.status(500).json(err);
+      }
+      else{
+        res.status(200).end();
+      }
+    });
+  });
 });
+
+
+router.put('/update/:id', function(req, res, next){
+  Cattle.findById(req.params.Id, function(err, cattle){
+    if(err){
+      res.status(500).json(err);
+    }
+    else{
+      cattle.update(req.body, function(err){
+        if(err){
+          res.status(500).json(err);
+        }
+        res.status(200).end();
+      });
+    }
+  })
+});
+
+router.get('/find/:name', function(req, res, next){
+  Cattle.find({name: req.params.name}, function(err, cattle){
+    if(err){
+      res.status(500).json(err);
+    }
+    if(!cattle.length){
+      res.status(404).send("Cattle not found");
+    }
+    else{
+      res.status(200).json(cattle);
+    }
+  });
+});
+
+router.post('/add', function(req, res, next){
+  var cattle = new Cattle(req.body);
+  cattle.save(function(err){
+    if(err){
+      res.status(500).json(err);
+    }
+    else{
+      res.status(201).json(cattle);
+    }
+  });
+});
+
 
 module.exports = router;
